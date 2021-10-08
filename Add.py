76a76bf19e8e3a4f18ae954beba0ef4b32cbd658
chanidul <chanidu@live.com>
2021-10-08 14:13:17 +0530

    first commit

---
+++ Add.py
@@ -0,0 +1,9 @@
+
+x = int(input("Enter first number"))
+y = int(input("Enter 2nd Number"))
+print(type(x))
+
+z = x + y
+print(z)
+
+3
---
+++ AplusB.py
@@ -0,0 +1,9 @@
+
+def sum_of_two_digits(first_digit, second_digit):
+    return first_digit + second_digit
+
+if __name__ == '__main__':
+    lis =(input(),input(),input())
+
+    print(sum_of_two_digits(int(lis[0]), int(lis[2])))
+
